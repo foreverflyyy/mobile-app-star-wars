995cc1e0866e7b9da516174eb9a5cacfeadc9019
Nikita <nikita.ushackow.02@gmail.com>
2023-08-06 23:58:13 +0200

    dev: start project with start dependencies

--- README.md
+++
@@ -1,1 +0,0 @@
-# mobile-app-star-wars
---
+++ starWarsApp/src/navigation/Navigations.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import {Text, View} from 'react-native';
+
+const Navigation = () => {
+    return (
+        <Text>
+            Navigation
+        </Text>
+    );
+};
+
+export default Navigation;
